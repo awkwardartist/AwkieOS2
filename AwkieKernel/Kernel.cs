7c07b96b1e2b72e43adae2168909b23c2aab9628
awkwardartist <73862490+awkwardartist@users.noreply.github.com>
2020-12-30 23:31:02 -0500

    Add project files.

---
+++ AwkieKernel/Kernel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace AwkieKernel
+{
+    public class Kernel : Sys.Kernel
+    {
+        protected override void BeforeRun()
+        {
+            Console.WriteLine("Boot Success!!");
+        }
+
+        protected override void Run()
+        {
+            Console.WriteLine("Welcome to AwkieOS!");
+        Top:
+            Console.Write(">>");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "help":
+                    Console.WriteLine("I'm here to help! Here are some available commands:");
+                    break;
+                case "gui":
+
+                    break;
+                default:
+                    Console.WriteLine("invalid or mistyped command");
+                    break;
+            }
+                
+            goto Top;
+        }
+    }
+}
