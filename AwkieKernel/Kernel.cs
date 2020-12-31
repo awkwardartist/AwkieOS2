8d1ad32283e6f7d23f4051812d4bd395266fb5a6
awkwardartist <73862490+awkwardartist@users.noreply.github.com>
2020-12-31 00:39:44 -0500

    messing with partitioning

--- AwkieKernel/Kernel.cs
+++ AwkieKernel/Kernel.cs
@@ -1,37 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Windows.Forms;
 using System.Text;
+using System.IO;
+using System.Threading;
 using Sys = Cosmos.System;
 
 namespace AwkieKernel
 {
     public class Kernel : Sys.Kernel
     {
+        public Sys.FileSystem.FileSystemFactory systemFactory;
         protected override void BeforeRun()
         {
+            Console.Clear();
             Console.WriteLine("Boot Success!!");
-        }
+            
 
+        }
+        
         protected override void Run()
         {
-            Console.WriteLine("Welcome to AwkieOS!");
-        Top:
-            Console.Write(">>");
-            var input = Console.ReadLine();
-            switch (input)
+            Devices:
+            Console.WriteLine("I detect: " + Cosmos.HAL.BlockDevice.Partition.Devices.Count + " partition devices, please choose\none for your filesystem to go in:");
+            foreach(var part in Cosmos.HAL.BlockDevice.Partition.Devices)
             {
-                case "help":
-                    Console.WriteLine("I'm here to help! Here are some available commands:");
-                    break;
-                case "gui":
-
-                    break;
-                default:
-                    Console.WriteLine("invalid or mistyped command");
-                    break;
+                Console.WriteLine(part);
             }
-                
-            goto Top;
+
+            Console.Write(">>");
+            Cosmos.HAL.BlockDevice.BlockDevice blockdevice = Cosmos.HAL.BlockDevice.Partition.Devices[0];
+            string name = Console.ReadLine();
+            bool def = true;
+            foreach (var part in Cosmos.HAL.BlockDevice.Partition.Devices)
+            {
+                if (name == part.ToString())
+                {
+                    blockdevice = part;
+                    def = false;
+                }
+            }
+            if (def)
+            {
+            def:
+                Console.WriteLine("your name didnt match any listed block devices,\nretry or go with default (" + Cosmos.HAL.BlockDevice.Partition.Devices[0] + ") type retry / cont");
+                Console.Write(">>");
+                var input = Console.ReadLine();
+                if(input == "retry")
+                {
+                    goto Devices;
+                } else if(input == "cont")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("type retry or cont!!");
+                }
+            }
+
+            Console.WriteLine("Formatting drive... this will wipe it. Continue?? (y/n)");
+            Console.Write(">>");
+
+            
+            if(Console.ReadLine() == "n")
+            {
+                Console.Clear();
+                goto Devices;
+            }
+
+
+
+
         }
     }
 }
